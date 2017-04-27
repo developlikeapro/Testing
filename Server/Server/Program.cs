a5d04de2ff0c93912076cfe6cde5447f8b5ebee7
Izma Khalid Qureshi <izmaqureshi@gmail.com>
2017-04-27 17:29:07 +0500

    Add files via upload

---
+++ Server/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Loopback, 11000);
+            s.Bind(ipend);
+            s.Listen(10);
+
+            Socket ss = s.Accept();
+            Console.WriteLine("Client Connected");
+            x:
+            Console.WriteLine("Enter 1 to Receive 2 to Send and 3 to Close");
+            
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1)
+            {
+                Console.WriteLine("Msg Receive!");
+                byte[] b = new byte[1024];
+
+                int count = ss.Receive(b);
+                Console.WriteLine(ASCIIEncoding.ASCII.GetString(b, 0, count));
+               // ss.Close();
+                goto x;
+                
+            }
+           
+            else
+            {
+                ss.Close();
+            }
+        }
+    }
+}
